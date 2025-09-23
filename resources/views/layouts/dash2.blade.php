99ab6a9eae89eaa3690018ffc20b9c235008371c
blueswayne133 <blueswayne133@gmail.com>
2025-09-23 15:28:45 -0700

    new feature

--- resources/views/layouts/dash2.blade.php
+++ resources/views/layouts/dash2.blade.php
@@ -307,6 +307,21 @@
     @laravelPWA
 </head>
 
+
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+    var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'b0039885101b8b8e90d5739ca33a57051f68973c';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+
+
 <body class="bg-gray-50">
     <!-- Modern Page Loader -->
     <div class="page-loading active">
